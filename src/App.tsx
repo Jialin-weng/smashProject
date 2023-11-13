897574902569b93a37384148500404931fe25055
Jialin Weng <101737864+Jialin-weng@users.noreply.github.com>
2023-11-13 02:00:36 -0500

    revert

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Message from './Message'
-import NavigationBar from './navbar';
-
-function App() {
-  return  <div><NavigationBar></NavigationBar></div>;
-}
-
-export default App;
