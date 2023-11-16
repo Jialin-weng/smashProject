6476ec5a5f4faf80fe4578f01e3b02f92e9e899c
Jialin Weng <101737864+Jialin-weng@users.noreply.github.com>
2023-11-16 16:11:53 -0500

    Update findMatch.php

--- findMatch.php
+++ findMatch.php
@@ -49,7 +49,7 @@
     <h1>findMatch</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
