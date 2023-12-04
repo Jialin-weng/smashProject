ecb781ddfed0d5cb93bf195a69a2bacf531ec1a6
Jialin Weng <101737864+Jialin-weng@users.noreply.github.com>
2023-12-04 04:31:31 -0500

    Update search.php

--- search.php
+++ search.php
@@ -5,19 +5,14 @@ require("db_functions.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!empty($_POST['searchUsername'])) {
-        // Sanitize the input to prevent SQL injection
         $searchedUsername = htmlspecialchars($_POST['searchUsername']);
 
-        // Search for the user by username
         $foundUser = getUserByUsername($searchedUsername);
 
-        // Check if the user is found
         if ($foundUser) {
-            // Redirect to the user's profile page or display information as needed
             header("Location: profile.php?username=" . $searchedUsername);
             exit;
         } else {
-            // If the user is not found, you can redirect or display a message
             $_SESSION['search_error'] = "User not found";
             header("Location: index.php");
             exit;
